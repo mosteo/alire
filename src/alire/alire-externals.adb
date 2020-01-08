d1558ff07d7eed45277b07e9e2a2c46e0c716368
Alejandro R. Mosteo <amosteo@unizar.es>
2020-01-08 13:17:28 +0100

    Unindexed externals support
    These externals serve to represent a crate that is known to exist,
    but is not yet properly packaged in Alire.

--- src/alire/alire-externals.adb
+++ src/alire/alire-externals.adb
@@ -1,3 +1,5 @@
+with Alire.Externals.Unindexed;
+
 with TOML;
 
 package body Alire.Externals is
@@ -21,6 +23,32 @@ package body Alire.Externals is
    ---------------
 
    function From_TOML (From : TOML_Adapters.Key_Queue) return External'Class is
-     (raise Unimplemented); -- No concrete externals defined yet
+      use type TOML.TOML_Value;
+   begin
+
+      --  An empty value is the particular case of unindexed external (we known
+      --  it exists, but nothing else).
+
+      if From.Unwrap = TOML.No_TOML_Value or else
+        (From.Unwrap.Kind in TOML.TOML_Table and then
+         From.Unwrap.Keys'Length = 0)
+      then
+         return Unindexed.External'(null record);
+      elsif From.Unwrap.Kind not in TOML.TOML_Table then
+         From.Checked_Error ("external description must be a TOML table"
+                             & " but got a " & From.Unwrap.Kind'Img);
+      end if;
+
+      --  No other externals defined yet:
+      From.Checked_Error ("invalid external description");
+
+   exception
+      when Checked_Error =>
+         raise;
+      when E : others =>
+         Log_Exception (E);
+         From.Checked_Error
+           ("invalid external description (see details with -d)");
+   end From_TOML;
 
 end Alire.Externals;
