5620a8fc8a2b4697279397cd3ceea7f12cae90ec
Alejandro R. Mosteo <amosteo@unizar.es>
2024-11-10 19:00:53 +0100

    wip: on track to 1st prototype

---
+++ support/embedder/embedder.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# Harden the script
+
+trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR
+trap 'echo "Interrupted" >&2 ; exit 1' INT
+
+set -o errexit
+set -o nounset
+
+# Build awsres from AWS only if awsres is not yet available here
+
+if [ ! -f awsres ]; then
+    workdir=$PWD
+    tmp=$(mktemp -d)
+    pushd "$tmp"
+
+    alr get --build aws^24
+    find . -name awsres -exec cp {} "$workdir" \;
+
+    # Clean up
+    popd
+    rm -rf "$tmp"
+fi
+
+# Clean up
