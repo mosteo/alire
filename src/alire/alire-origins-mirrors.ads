058967ceee2c70d147e15f4a26eedaa9baa55f23
Alejandro R. Mosteo <amosteo@unizar.es>
2025-04-15 22:29:59 +0200

    wip: deserialization

--- src/alire/alire-origins-mirrors.ads
+++ src/alire/alire-origins-mirrors.ads
@@ -3,18 +3,18 @@ with Alire.TOML_Adapters;
 
 package Alire.Origins.Mirrors is
 
-   subtype Mirrored_Kinds is Origins.Kinds with
+   subtype Mirror_Kinds is Origins.Kinds with
      Predicate =>
-       Mirrored_Kinds in VCS_Kinds | Source_Archive | Binary_Archive;
+       Mirror_Kinds in VCS_Kinds | Source_Archive | Binary_Archive;
 
    type Mirror is tagged private;
 
-   function Kind (This : Mirror) return Kinds;
+   function Kind (This : Mirror) return Mirror_Kinds;
 
    function URL (This : Mirror) return Alire.URL;
 
    function Whenever (This : Mirror; Env : Properties.Vector)
-   return Mirror;
+                      return Mirror;
 
    function Is_Available (This : Mirror; Env : Properties.Vector)
             return Boolean;
@@ -32,16 +32,16 @@ private
      (Values => Archive_Data,
       Image  => Alire.Origins.Binary_Image);
 
-   type Mirror_Data (Kind : Mirrored_Kinds := Git) is record
+   type Mirror_Data (Kind : Mirror_Kinds := Git) is record
       Name : Mirror_Name;
       case Kind is
-         when Alire.Origins.Binary_Archive =>
+         when Binary_Archive =>
             Binary_Mirror : Conditional_Archive;
 
          when VCS_Kinds =>
             Repo_URL : Unbounded_String;
 
-         when Alire.Origins.Source_Archive =>
+         when Source_Archive =>
             Src_URL : Unbounded_String;
 
          when others =>
@@ -51,6 +51,10 @@ private
 
    type Mirror is tagged record
       Data : Mirror_Data;
+      --  Unserialized information
+
+      Obj  : TOML.TOML_Value;
+      --  The original TOML object, for To_TOML
    end record;
 
 end Alire.Origins.Mirrors;
